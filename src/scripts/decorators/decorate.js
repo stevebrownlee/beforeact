de2a1c59fd43f7fe5ad7ac60f2d1bdb8b5a4ec48
Steve Brownlee <chortlehoort@gmail.com>
2018-05-10 08:35:35 -0500

    Lots of comments in main.js

--- src/scripts/decorators/decorate.js
+++ src/scripts/decorators/decorate.js
@@ -1,25 +1,29 @@
-const _result = Symbol()
-const _private = new Map()
-_private.set(_result, {})
+const decorator = () => {
+    const _result = Symbol()
+    const _private = new Map()
+    _private.set(_result, {})
 
-const decorate = Object.create(null, {
-    init: {
-        value: function (obj) {
-            this._ = obj
-            return this
+    const decorate = Object.create(null, {
+        init: {
+            value: function (obj) {
+                _private.set(_private.get(_result), obj)
+                return this
+            }
+        },
+        with: {
+            value: function (decoration) {
+                _private.set(Object.assign(_private.get(_result), decoration))
+                return this
+            }
+        },
+        done: {
+            value: function () {
+                return _private.get(_result)
+            }
         }
-    },
-    with: {
-        value: function (decoration) {
-            this._ = Object.assign(this._, decoration)
-            return this
-        }
-    },
-    done: {
-        value: function () {
-            return this._
-        }
-    }
-})
+    })
 
-module.exports = decorate
+    return decorate
+}
+
+module.exports = decorator
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,32 +2,42 @@ const { factory, div, article, section, p, h1, header } = require("./components"
 const { decorate, clickable, draggable, droppable, highlighter } = require("./decorators")
 const juan = require("./data/juan")
 
-/* eslint no-undef: "off" */
+// Simple component with text content
 const Title = h1("Letters from a Stoic")
-
 
 /*
 * If the second argument is an HTMLElement, it will
 * be appended as a child of the containing element
 *
+*/
+const ArticleHeader = header(
+    // Props for the <header> component
+    { className: "article__header", id: "articleHeader" },
+
+    // Title component will be a child component
+    Title
+)
+
+/*
 * This example uses composition to include mouse event handlers
 */
 const ArticleHeader = header(
-    decorate.init({ className: "article__header", id: "articleHeader" })
+    decorate().init({ className: "article__header", id: "articleHeader" })
             .with(highlighter.init("highlight--goldenrod"))
-            .with(clickable.init(() => console.log("Hi")))
+            .with(clickable.init(function () { console.log(this.textContent) } ))
             .with(draggable.init("class"))
             .done(),
     Title
 )
 
 /**
- * This component has no children
+ * This component has no children, just text content
  *
  * Directly defining mouse event handlers instead of composing
  */
 const ArticleQuote = section(
-    Object.assign({
+    // Props for the <section> component
+    {
         className: "article__section",
         onmouseover: function () {
             this.classList.add("highlight--smoke")
@@ -35,16 +45,28 @@ const ArticleQuote = section(
         onmouseout: function () {
             this.classList.remove("highlight--smoke")
         }
-    }),
+    },
     "It is not the man who has too little that is poor, but the one who hankers after more."
 )
 
-// I can specify as many children as I want for a component
+/*
+    I can specify as many children as I want for a component
+*/
 const Article = article(
+    // Props for the <article> component
     { className: "article" },
+
+    // Two children components
     ArticleHeader,
     ArticleQuote
 )
+
+/*
+    The `render()` method on the component factory takes two arguments
+        1. The existing target DOM component in which the new component
+           will be rendered.
+        2. The component to be rendered
+*/
 factory.render("#simple-components", Article)
 
 
@@ -53,7 +75,11 @@ factory.render("#simple-components", Article)
  * Build a component that has multiple <section> child components,
  * one for each key on the `juan` object.
  */
-const PatientProperties = div({},
+const PatientProperties = div(
+    // Props for the <div> component. No specific props, so empty object.
+    {},
+
+    // Render each property of `juan` as a child <section> component
     Object.keys(juan)
           .map(key => {
               return section({}, `${key}: ${juan[key]}`)
@@ -65,10 +91,16 @@ const PatientProperties = div({},
  * component as it's child
  */
 const Juan = article(
-    decorate.init({ id: "juan", className: "patientDetails" })
+    /*
+        Decorate the base props object with the `droppable` decoration
+        so that any draggable element can be dropped in it
+    */
+    decorate().init({ id: "juan", className: "patientDetails" })
             .with(droppable.init())
             .done()
     ,
+
+    // The <div> we defined above will be a child of this <article>
     PatientProperties
 )
 
@@ -77,8 +109,8 @@ const Juan = article(
  * Build a <div> component with an <h1> element as its child
  */
 const Header = div(
-    { className: "patientHeader" },
-    h1("Patients")
+    { className: "patientHeader" },  // Props of <div>
+    h1("Patients")  // Child <h1> element of <div>
 )
 
 /**
@@ -88,17 +120,16 @@ const Header = div(
  *
  * They will be siblings to each other
  */
-const Page = article(
-    { className: "container patientList" },
-    Header,
-    Juan
+const Patient = article(
+    { className: "container patientList" },  // Props of <article>
+    Header, Juan   // Two child components
 )
 
 
 /**
- * Render the Page component to a specific DOM element
+ * Render the Patient component to a specific DOM element
  */
-factory.render("#array-components", Page)
+factory.render("#array-components", Patient)
 
 
 
